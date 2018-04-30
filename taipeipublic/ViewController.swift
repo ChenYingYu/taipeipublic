9d815a16003eacdac6b876330175baa27153c93d
ChenYingYu <alan831007@gmail.com>
2018-04-30 15:51:01 +0800

    Add .gitignore. Show basic GoogleMap and user location

--- taipeipublic/ViewController.swift
+++ taipeipublic/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupMap()
     }
 
-
+    func setupMap() {
+        let camera = GMSCameraPosition.camera(withLatitude: 25.042416, longitude: 121.564793, zoom: 15.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+    }
 }
 
