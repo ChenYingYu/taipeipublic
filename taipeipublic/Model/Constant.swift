fd8edecbd91c69bec8eeea5938974acd82105397
ChenYingYu <alan831007@gmail.com>
2018-05-02 11:30:36 +0800

    Create MVC folders.

---
+++ taipeipublic/Model/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  taipeipublic
+//
+//  Created by ChenAlan on 2018/4/30.
+//  Copyright © 2018年 ChenAlan. All rights reserved.
+//
+
+import Foundation
+
+class Constant {
+    
+    static let googleMapsAPIKey = "AIzaSyAGVKnhXAE-eiDJuTK3i-91rSnpngQAkHI"
+    static let googlePlacesAPIKey = "AIzaSyC07iM6JrYRi4fvQVybWRLYQthE1f7ITf8"
+}
