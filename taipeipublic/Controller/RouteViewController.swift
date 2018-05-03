4fa2dd3032536eebf3c311e7b6a7543fe02a3c93
ChenYingYu <alan831007@gmail.com>
2018-05-03 16:42:59 +0800

    Show RouteViewController and dismiss.

---
+++ taipeipublic/Controller/RouteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RouteViewController.swift
+//  taipeipublic
+//
+//  Created by ChenAlan on 2018/5/3.
+//  Copyright © 2018年 ChenAlan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RouteViewController: UIViewController {
+    @IBAction func back(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
