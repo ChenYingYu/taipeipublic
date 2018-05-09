625a4e1d602fe28ed507a34168b02ff4f8be47c8
ChenYingYu <alan831007@gmail.com>
2018-05-09 17:28:31 +0800

    Move function "checkNearbyStation" from MapViewController to YoubikeManager

--- taipeipublic/Model/YoubikeManager.swift
+++ taipeipublic/Model/YoubikeManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import GoogleMaps
 
 struct YoubikeStation: Codable {
     let name: String
@@ -50,8 +51,38 @@ struct YoubikeManager: Codable {
         }
         return stations
     }
-}
-
-struct YoubikeMarker {
-    
+    func checkNearbyStation(position: CLLocationCoordinate2D) -> [YoubikeStation] {
+        var nearbyStations = [YoubikeStation]()
+        if let youbikeManager = YoubikeManager.getStationInfo(), let stations = youbikeManager.getYoubikeLocation() {
+            for index in stations.indices {
+                if let latitude = Double(stations[index].latitude), let longitude = Double(stations[index].longitude) {
+                    if self.getdistance(lng1: position.longitude, lat1: position.latitude, lng2: Double(longitude), lat2: Double(latitude)) < 300.0 {
+                        print("= = = Got a Youbike Station Near User = = =\n")
+                        print("= = = = = \(index) = = = = = =\n")
+                        nearbyStations.append(stations[index])
+                    }
+                } else {
+                    print("===============")
+                    print("ERROR Number: \(index)")
+                    print("ERROR Latitude: \(stations[index].latitude)")
+                    print("ERROR Longitude: \(stations[index].longitude)")
+                    print("===============")
+                }
+            }
+        }
+        return nearbyStations
+    }
+    func getdistance(lng1: Double, lat1: Double, lng2: Double, lat2: Double) -> Double {
+        //將角度轉為弧度
+        let radLat1 = lat1 * Double.pi / 180
+        let radLat2 = lat2 * Double.pi / 180
+        let radLng1 = lng1 * Double.pi / 180
+        let radLng2 = lng2 * Double.pi / 180
+        let tmp1 = radLat1 - radLat2
+        let tmp2 = radLng1 - radLng2
+        //計算公式
+        let distance = 2 * asin(sqrt(pow(sin(tmp1 / 2), 2)+cos(radLat1) * cos(radLat2) * pow(sin(tmp2 / 2), 2))) * 6378.137 * 1000
+        //單位公尺
+        return distance
+    }
 }
