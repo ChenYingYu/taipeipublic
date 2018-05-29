dc7cd22d8f79a7cda1b0c5a52e7e597bb8bbd1cd
ChenYingYu <alan831007@gmail.com>
2018-05-29 15:47:39 +0800

    Move PTXAppID and PTXAppKey to Constant.swift

--- taipeipublic/Model/Constant.swift
+++ taipeipublic/Model/Constant.swift
@@ -11,4 +11,6 @@ import Foundation
 class Constant {
     static let googleMapsAPIKey = "AIzaSyAGVKnhXAE-eiDJuTK3i-91rSnpngQAkHI"
     static let googlePlacesAPIKey = "AIzaSyC07iM6JrYRi4fvQVybWRLYQthE1f7ITf8"
+    static let PTXAppID = "6682cd802e7f48cb903f21ed478943e1"
+    static let PTXAppKey = "-Fjkt5T-AtetVWGUIC6F1ZSngmI"
 }
