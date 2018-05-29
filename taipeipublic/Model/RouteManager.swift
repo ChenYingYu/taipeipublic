423c2b2eaa584278d9769791461a68b54b198f74
ChenYingYu <alan831007@gmail.com>
2018-05-29 14:37:27 +0800

    Import CommonCrypto and requestBusInfo by routeManager

--- taipeipublic/Model/RouteManager.swift
+++ taipeipublic/Model/RouteManager.swift
@@ -191,4 +191,81 @@ class RouteManager {
             }
         }
     }
+
+    func requestBusRoute() {
+        let base64String = "x-date: Tue, 29 May 2018 06:22:50 GMT".hmac(algorithm: HMACAlgorithm.SHA1, key: "-Fjkt5T-AtetVWGUIC6F1ZSngmI")
+
+        let headers = [
+            "Authorization": "hmac username=\"6682cd802e7f48cb903f21ed478943e1\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"\(base64String)\"",
+            "x-date": "Tue, 29 May 2018 06:22:50 GMT"
+            ]
+
+        let urlParams = [
+            "$format": "JSON"
+            ]
+
+        Alamofire.request("http://ptx.transportdata.tw/MOTC/v2/Bus/StopOfRoute/City/Taipei/212%E7%9B%B4", method: .get, parameters: urlParams, headers: headers)
+            .validate()
+            .responseJSON { response in
+                if response.result.error == nil {
+                    debugPrint("HTTP Response Body: \(response.result.value)")
+                } else {
+                    debugPrint("HTTP Request failed: \(response.result.error)")
+                }
+        }
+    }
+}
+
+enum HMACAlgorithm {
+    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+
+    func toCCHmacAlgorithm() -> CCHmacAlgorithm {
+        var result: Int = 0
+        switch self {
+        case .MD5:
+            result = kCCHmacAlgMD5
+        case .SHA1:
+            result = kCCHmacAlgSHA1
+        case .SHA224:
+            result = kCCHmacAlgSHA224
+        case .SHA256:
+            result = kCCHmacAlgSHA256
+        case .SHA384:
+            result = kCCHmacAlgSHA384
+        case .SHA512:
+            result = kCCHmacAlgSHA512
+        }
+        return CCHmacAlgorithm(result)
+    }
+
+    func digestLength() -> Int {
+        var result: CInt = 0
+        switch self {
+        case .MD5:
+            result = CC_MD5_DIGEST_LENGTH
+        case .SHA1:
+            result = CC_SHA1_DIGEST_LENGTH
+        case .SHA224:
+            result = CC_SHA224_DIGEST_LENGTH
+        case .SHA256:
+            result = CC_SHA256_DIGEST_LENGTH
+        case .SHA384:
+            result = CC_SHA384_DIGEST_LENGTH
+        case .SHA512:
+            result = CC_SHA512_DIGEST_LENGTH
+        }
+        return Int(result)
+    }
+}
+
+extension String {
+    func hmac(algorithm: HMACAlgorithm, key: String) -> String {
+        let cKey = key.cString(using: String.Encoding.utf8)
+        let cData = self.cString(using: String.Encoding.utf8)
+        var result = [CUnsignedChar](repeating: 0, count: Int(algorithm.digestLength()))
+        CCHmac(algorithm.toCCHmacAlgorithm(), cKey!, Int(strlen(cKey!)), cData!, Int(strlen(cData!)), &result)
+        let hmacData: NSData = NSData(bytes: result, length: (Int(algorithm.digestLength())))
+        let hmacBase64 = hmacData.base64EncodedString(options: NSData.Base64EncodingOptions.lineLength76Characters)
+        return String(hmacBase64)
+    }
 }
