418e274c860524fa213a53f652bb3b2830bcd287
ChenYingYu <alan831007@gmail.com>
2018-05-30 12:39:32 +0800

    Tag if a bus stop is depatureStop or arrivalStop in BusInfoViewController

--- taipeipublic/View/BusStopInfoTableViewCell.swift
+++ taipeipublic/View/BusStopInfoTableViewCell.swift
@@ -11,9 +11,13 @@ import UIKit
 class BusStopInfoTableViewCell: UITableViewCell {
 
     @IBOutlet weak var stopNameLabel: UILabel!
+    @IBOutlet weak var departureOrArrivalTagLabel: UILabel!
+    let departureColor =  UIColor(red: 10.0/255.0, green: 140.0/255.0, blue: 204.0/255.0, alpha: 1.0)
+    let arrivalColor = UIColor(red: 4.0/255.0, green: 52.0/255.0, blue: 76.0/255.0, alpha: 1.0)
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        departureOrArrivalTagLabel.isHidden = true
     }
 }
