8541fa5f84efbf9688427238f1518976825bd8f9
ChenYingYu <alan831007@gmail.com>
2018-05-30 10:30:10 +0800

    Create BusStopInfoTableViewCell for busStopInfoTableView

---
+++ taipeipublic/View/BusStopInfoTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BusStopInfoTableViewCell.swift
+//  taipeipublic
+//
+//  Created by ChenAlan on 2018/5/30.
+//  Copyright © 2018年 ChenAlan. All rights reserved.
+//
+
+import UIKit
+
+class BusStopInfoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var stopNameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
